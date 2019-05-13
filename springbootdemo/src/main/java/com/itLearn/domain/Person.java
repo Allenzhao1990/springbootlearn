5d58d742617186bec6560eaa7df508ed9f6cc6b8
zhaowei <hgzhaowei@163.com>
2019-05-13 19:45:42 +0800

    :white_check_mark:增加lombok

--- springbootdemo/src/main/java/com/itLearn/domain/Person.java
+++ springbootdemo/src/main/java/com/itLearn/domain/Person.java
@@ -1,9 +1,12 @@
 package com.itLearn.domain;
 
+import lombok.Data;
+
 /**
  * Created by zhaowei on 2019/5/9.
  */
 
+@Data
 public class Person {
 
     private int id;
@@ -14,45 +17,5 @@ public class Person {
 
     private String addr;
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getAddr() {
-        return addr;
-    }
-
-    public void setAddr(String addr) {
-        this.addr = addr;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", addr='" + addr + '\'' +
-                '}';
-    }
 }
